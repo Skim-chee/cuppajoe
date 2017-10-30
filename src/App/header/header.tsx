7b753f488828eb71764152fb7062c0594e8dd3ff
skim-chee <kim.seean@gmail.com>
2017-10-29 23:05:31 -0400

    Changed logo to link, re-centered and added media query, TODO: Work on global variable css

--- src/App/header/header.tsx
+++ src/App/header/header.tsx
@@ -1,14 +1,13 @@
 import React from 'react';
 import styles from './styles';
 const logo = require('../../Assets/Logo@2x.png');
-// import logo from '../../Assets/Logo@2x.png';
 
 const Header = () => {
 	return (
 		<header>
 			<div className={styles.container}>
-				<a href="/">
-					<img className={styles.logoWeb} src={logo} alt="logo" />
+				<a className={styles.logoLink} href="/">
+					<img className={styles.logoImg} src={logo} alt="logo" />
 				</a>
 			</div>
 		</header>
--- src/App/header/styles.tsx
+++ src/App/header/styles.tsx
@@ -1,13 +1,19 @@
-import { style } from 'typestyle';
+import { style, media } from 'typestyle';
 
 namespace styles {
-	export const container = style({
-		display: 'flex',
-		flexDirection: 'column',
-		width: '100%'
+	export const container = style(
+		{
+			display: 'flex',
+			flexDirection: 'column',
+			width: '100%',
+			marginTop: '48px'
+		},
+		media({ minWidth: 0, maxWidth: 640 }, { marginTop: '32px' })
+	);
+	export const logoLink = style({
+		alignSelf: 'center'
 	});
-	export const logoWeb = style({
-		alignSelf: 'center',
+	export const logoImg = style({
 		height: '148px'
 	});
 }
