70c1c3dbf0fd8cc5e8d4b62bf2255b966ce56cfb
skim-chee <kim.seean@gmail.com>
2017-11-07 00:31:20 -0500

    Added Radio buttons and images. Time to add functionality and active states

--- src/App/optionButton/optionButton.tsx
+++ src/App/optionButton/optionButton.tsx
@@ -1,4 +1,9 @@
 import React from 'react';
+import { style } from 'typestyle';
+const chill = require('../../Assets/Options/Chill@2x.png');
+const go = require('../../Assets/Options/Go@2x.png');
+const productive = require('../../Assets/Options/Productive@2x.png');
+const stay = require('../../Assets/Options/Stay@2x.png');
 
 export interface Props {
 	feeling: string;
@@ -7,10 +12,44 @@ export interface Props {
 
 const OptionButton: React.SFC<Props> = props => {
 	const { feeling, option } = props;
+	let feelingImage;
+	switch (props.feeling) {
+		case 'chill':
+			feelingImage = chill;
+			break;
+		case 'productive':
+			feelingImage = productive;
+			break;
+		case 'stay':
+			feelingImage = stay;
+			break;
+		case 'go':
+			feelingImage = go;
+			break;
+		default:
+			break;
+	}
+
+	const buttonStyle = style({
+		backgroundImage: `url(${feelingImage})`
+	});
+
+	const btnImage = style({
+		cursor: 'pointer',
+		backgroundSize: 'contain',
+		backgroundRepeat: 'no-repeat',
+		display: 'inline-block',
+		width: '108px',
+		height: '108px',
+		transition: 'all 250ms ease-in',
+		filter: 'brightness(1.5) grayscale(1) opacity(.5)'
+	});
 
 	return (
 		<div>
-			{feeling} AND {option}
+			<input type="radio" value={feeling} />
+			<label className={[btnImage, buttonStyle].join(' ')} />
+			{option}
 		</div>
 	);
 };
